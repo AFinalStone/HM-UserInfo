8bc9a3e6b7829d807b6423e3c7f3cb63010d1015
houjinyun <houjinyun@gmail.com>
2018-09-27 11:44:22 +0800

    吐槽接入里面，判断头像是否为空

--- library/src/main/java/com/hm/iou/userinfo/business/view/TucaoActivity.java
+++ library/src/main/java/com/hm/iou/userinfo/business/view/TucaoActivity.java
@@ -3,6 +3,7 @@ package com.hm.iou.userinfo.business.view;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 
 import com.hm.iou.base.webview.BaseWebviewActivity;
 import com.hm.iou.network.HttpReqManager;
@@ -28,7 +29,14 @@ public class TucaoActivity extends BaseWebviewActivity {
 
         UserInfo userInfo = UserManager.getInstance(this).getUserInfo();
         String nickname = StringUtil.getUnnullString(userInfo.getNickName());
+        if (TextUtils.isEmpty(nickname)) {
+            nickname = "小管家";
+        }
         String headimgurl = userInfo.getAvatarUrl();
+        if (TextUtils.isEmpty(headimgurl)) {
+            //在百度上的条管家logo图片
+            headimgurl = "https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=5f3b3f0941086e067ea5371963611091/8435e5dde71190ef916431e7c31b9d16fdfa606f.jpg";
+        }
         String openid = userInfo.getUserId();
         StringBuilder sb = new StringBuilder();
         sb.append("nickname=").append(nickname)
