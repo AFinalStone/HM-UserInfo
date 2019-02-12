b30eb4712d4d601d5a46bb37a97b59258381b055
houjinyun <houjinyun@gmail.com>
2019-02-12 09:43:43 +0800

    绑定邮箱前，需要先做实名认证

--- library/src/main/java/com/hm/iou/userinfo/business/view/AboutEmailActivity.java
+++ library/src/main/java/com/hm/iou/userinfo/business/view/AboutEmailActivity.java
@@ -5,6 +5,7 @@ import android.text.TextUtils;
 
 import com.hm.iou.base.BaseActivity;
 import com.hm.iou.base.mvp.MvpActivityPresenter;
+import com.hm.iou.router.Router;
 import com.hm.iou.sharedata.UserManager;
 import com.hm.iou.sharedata.model.CustomerTypeEnum;
 import com.hm.iou.sharedata.model.UserInfo;
@@ -30,6 +31,16 @@ public class AboutEmailActivity extends BaseActivity {
     protected void initEventAndData(Bundle bundle) {
         UserInfo userInfo = UserManager.getInstance(this).getUserInfo();
         int type = userInfo.getType();
+
+        //以前绑定邮箱前，需要先做实名认证
+        if (type == CustomerTypeEnum.CSub.getValue() || type == CustomerTypeEnum.CPlus.getValue()) {
+            Router.getInstance()
+                    .buildWithUrl("hmiou://m.54jietiao.com/facecheck/authentication")
+                    .navigation(mContext);
+            finish();
+            return;
+        }
+
         //绑定过邮箱
         if ((type == CustomerTypeEnum.APlus.getValue() || type == CustomerTypeEnum.ASub.getValue())
                 && !TextUtils.isEmpty(userInfo.getMailAddr())) {
