c9372d18ca159db147253230964c38d8544468c1
石磊 <>
2019-07-31 17:52:35 +0800

    修改接口地址，登陆账号手机号

--- app/src/main/java/com/hm/iou/userinfo/demo/MainActivity.java
+++ app/src/main/java/com/hm/iou/userinfo/demo/MainActivity.java
@@ -55,8 +55,8 @@ public class MainActivity extends AppCompatActivity {
         String pwd = MD5.hexdigest("123456".getBytes());
 //        String pwd = MD5.hexdigest("kaka123ac".getBytes());
         MobileLoginReqBean reqBean = new MobileLoginReqBean();
-//        reqBean.setMobile("15267163669");
-        reqBean.setMobile("17681832816");
+        reqBean.setMobile("15267163669");
+//        reqBean.setMobile("17681832816");
 //        reqBean.setMobile("18969904897");
         reqBean.setQueryPswd(pwd);
         HttpReqManager.getInstance().getService(LoginService.class)
